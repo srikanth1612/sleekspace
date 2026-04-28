495b378c06234ddce863a102cc3b2c9da1890f11
srikanth1612 <nettemsrikanth16@gmail.com>
2026-04-28 12:59:58 +0530

    layout file changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,30 @@
+
 import "./globals.css";
 
-export const metadata = { title: "Sleekspace Interior" };
+export const metadata = {
+  title: "Sleekspace Interiors - Modern Interior Design",
+  description:
+    "Sleekspace Interiors offers modern, elegant, and functional interior design solutions with premium quality and affordable pricing.",
+  
+  keywords: [
+    "Interior Design",
+    "Home Interiors",
+    "Modular Kitchen",
+    "Residential Interiors",
+    "Commercial Interiors",
+    "Interior Designers in India",
+    "Interior Designers in Hyderabad",
+    "Interior Designers in Telangana",
+  ],
+
+  verification: {
+    google: "googlec2edbeb66fcc6610",
+  },
+
+  icons: {
+    icon: "/logo.jpeg",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -9,3 +33,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
